c9e645fed914cb26aec4333adbd076c94afd8c57
Dempsey845 <dempseynewton1@gmail.com>
2025-07-17 23:30:40 +0100

    Added FakeObstacle - to prevent sheep from wandering into the pen itself

---
+++ Assets/Scripts/Level/FakeObstacle.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class FakeObstacle : MonoBehaviour
+{
+    /* Prevents the sheep from walking into the pen by itself */
+
+    [SerializeField] private GameObject fakeObstacle;
+
+    private SheepStateMachine stateMachine;
+
+    private float checkStateOfSheepRate = 0.5f;
+    private float timer;
+
+    private void Start()
+    {
+        stateMachine = FindFirstObjectByType<SheepStateMachine>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > checkStateOfSheepRate)
+        {
+            HandleFakeObstacle();
+            timer = 0;
+        }
+    }
+
+    private void HandleFakeObstacle()
+    {
+        if (stateMachine == null) { return; }
+
+        ISheepState currentState = stateMachine.GetCurrentState();
+
+        fakeObstacle.SetActive(currentState is not SheepFollowState);
+    }
+}
