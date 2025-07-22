2698c983fe06ec203762c23dcdaf6e14396e2e9b
Dempsey845 <dempseynewton1@gmail.com>
2025-07-22 20:16:51 +0100

    Added a Level Complete Particle FX

--- Assets/Scripts/Pen/Pen.cs
+++ Assets/Scripts/Pen/Pen.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class Pen : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem levelCompleteParticleFX;
+
     private const string SHEEP_TAG = "Sheep";
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(SHEEP_TAG))
         {
+            levelCompleteParticleFX.Play();
             LevelManager.Instance.CompleteLevel();
         }
     }
